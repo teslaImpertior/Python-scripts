9a3a244170e78d8ef2790a6c28a6327fd39c6b1f
teslaImpertior <36782612+teslaImpertior@users.noreply.github.com>
2018-12-03 14:56:29 +1100

    Add files via upload

---
+++ Theano/testGPU.py
@@ -0,0 +1,30 @@
+#set THEANO_FLAGS="device=cuda"
+
+from theano import function, config, shared, tensor
+import numpy
+import time
+
+#check device name
+#from tensorflow.python.client import device_lib
+#print(device_lib.list_local_devices())
+define_macros=[('MS_WIN64',None)]
+
+vlen = 10 * 300 * 768  # 10 x #cores x # threads per core
+iters = 1000
+
+rng = numpy.random.RandomState(22)
+x = shared(numpy.asarray(rng.rand(vlen), config.floatX))
+f = function([], tensor.exp(x))
+print(f.maker.fgraph.toposort())
+t0 = time.time()
+for i in range(iters):
+    r = f()
+t1 = time.time()
+print("Looping %d times took %f seconds" % (iters, t1 - t0))
+print("Result is %s" % (r,))
+if numpy.any([isinstance(x.op, tensor.Elemwise) and
+              ('Gpu' not in type(x.op).__name__)
+              for x in f.maker.fgraph.toposort()]):
+    print('Used the cpu')
+else:
+    print('Used the gpu')
